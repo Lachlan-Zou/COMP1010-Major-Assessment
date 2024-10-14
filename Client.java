564ed25a5ee03c148637a7f6c94507dbff1826a0
Tonnyton <tontapuk.charoenpol1@students.mq.edu.au>
2024-10-14 15:24:59 +1100

    Update Client.java

--- Client.java
+++ Client.java
@@ -13,28 +13,28 @@ public class Client {
                 System.out.println("You selected Human.");
                 // Modify the program logic for Human
                 Character Human = new Character(16,16,18);
-                System.out.println("Human: Hp " + Human.healthpoint + " | Str " +Human.strenght + " | Def " + Human.defence);
+                System.out.println("Human: Hp " + Human.healthpoint + " | Str " +Human.strength + " | Def " + Human.defence);
                 // Modify the stats
                 break;
             case 2:
                 System.out.println("You selected Orc.");
                 // Modify the program logic for Orc
                 Character Orc = new Character(18, 20, 12);
-                System.out.println("Orc: Hp " + Orc.healthpoint + " | Str " +Orc.strenght + " | Def " + Orc.defence);
+                System.out.println("Orc: Hp " + Orc.healthpoint + " | Str " +Orc.strength + " | Def " + Orc.defence);
                  // Modify the stats
                 break;
             case 3:
                 System.out.println("You selected Elf.");
                 // Modify the program logic for Elf
                 Character Elf = new Character(14, 14, 22);
-                System.out.println("Elf: Hp " + Elf.healthpoint + " | Str " +Elf.strenght + " | Def " + Elf.defence);
+                System.out.println("Elf: Hp " + Elf.healthpoint + " | Str " +Elf.strength + " | Def " + Elf.defence);
                  // Modify the stats
                 break;
             case 4:
                 System.out.println("You selected Dwarf.");
                 // Modify the program logic for Dwarf
                 Character Dwarf = new Character(15, 18, 17);
-                System.out.println("Dwarf: Hp " + Dwarf.healthpoint + " | Str " +Dwarf.strenght + " | Def " + Dwarf.defence);
+                System.out.println("Dwarf: Hp " + Dwarf.healthpoint + " | Str " +Dwarf.strength + " | Def " + Dwarf.defence);
                  // Modify the stats
                 break;
             default:
