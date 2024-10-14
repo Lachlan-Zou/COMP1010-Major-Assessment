e795f195d43ede54fd465fcad39159a1dff584db
Tonnyton <tontapuk.charoenpol1@students.mq.edu.au>
2024-10-14 14:38:39 +1100

    Create Character.java

---
+++ Character.java
@@ -0,0 +1,13 @@
+public class Character {
+    int healthpoint;
+    int strenght;
+    int defence;
+
+    public Character( int healthpoint, int strength, int defence){
+        this.healthpoint=healthpoint;
+        this.strenght = strength;
+        this.defence = defence;
+
+    }
+    
+}
