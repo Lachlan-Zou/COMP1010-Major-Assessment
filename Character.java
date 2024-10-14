2fa47a6467fd3b9b774f796393cdcf5cd39374a8
Tonnyton <tontapuk.charoenpol1@students.mq.edu.au>
2024-10-14 15:25:01 +1100

    Update Character.java

--- Character.java
+++ Character.java
@@ -1,13 +1,26 @@
 public class Character {
     int healthpoint;
-    int strenght;
+    int strength;
     int defence;
 
     public Character( int healthpoint, int strength, int defence){
         this.healthpoint=healthpoint;
-        this.strenght = strength;
+        this.strength = strength;
         this.defence = defence;
 
+    }
+
+    public void attack(Character enemy){
+        //damage based on characters strength and enemy defence
+        int damage = this.strength - enemy.defence;
+        if(damage > 0){
+            enemy.healthpoint -= damage;
+            System.out.println(this + " attacked " + enemy + " causing " + damage + " damage.");
+
+        }
+        else{
+            System.out.println(enemy + " blocked the attack!");
+        }
     }
     
 }
