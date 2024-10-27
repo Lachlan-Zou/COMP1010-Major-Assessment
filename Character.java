e87735f66bc1eeaf53fa6bad59b352a61bfc949f
Lachlan-Zou <lachlan.zou@students.mq.edu.au>
2024-10-27 20:26:20 +1100

    Adding flavour text

--- Character.java
+++ Character.java
@@ -47,7 +47,7 @@ public class Character {
             this.healthpoint -= damage;
             System.out.println(enemy.playerName + " attacked " + this.playerName + " causing " + damage + " damage.");    
             if (this.healthpoint<=0) {
-                // Put down the team removal here.
+                System.out.println(this.playerName+ " has been killed!");
             }
         }
         else{
@@ -59,15 +59,17 @@ public class Character {
         int diceRoll = dice.rollDice();  // Get a random dice roll
         int baseDamage = this.strength - enemy.defence;  // Base damage calculation (Strength - Defence)
         int damage = baseDamage + diceRoll;  // Final damage after adding dice roll
+
         if (damage <= 0) {
             damage = 1;  // Set minimum damage to 1
         }
+        
         boolean dodgeHit = false;
         if (enemy.speed>this.speed) {
             this.healthpoint -=damage;
             System.out.println(this.playerName+" was too slow! "  + damage + " was dealt.");
             if (this.healthpoint<=0) {
-                // Put down the team removal here.
+                System.out.println(this.playerName+ " has been killed!");
             }
         }
         for (int i = 0; i <5-this.speed; i++) {
@@ -82,13 +84,13 @@ public class Character {
             this.healthpoint -= damage/2;
             System.out.println(this.playerName+ " took " + damage/2);
             if (this.healthpoint<=0) {
-                // Put down the team removal here.
+                System.out.println(this.playerName+ " has been killed!");
             }
         }
         else {
             System.out.println(this.playerName+ " successfully dodges!");
-            enemy.healthpoint -= damage*2;
-            System.out.println(this.playerName+ " counterattacks! dealing " + damage*2 + " damage!");
+            enemy.healthpoint -= damage*1.5;
+            System.out.println(this.playerName+ " counterattacks! dealing " + damage*1.5 + " damage!");
             if (enemy.healthpoint<=0) {
                 enemyTeam.remove(enemy);
                 System.out.println(enemy.playerName + " Has been killed!");
